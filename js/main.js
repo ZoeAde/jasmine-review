f0eb1d86e31ee3822f668fb205cccde4912cf2a3
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-10 10:35:11 -0600

    adding solution to problem 4

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-// Write a function that takes three random numbers from 0 to 100 and returns the largest one.
 
+//Problem 1
+// Write a function that takes three random numbers from 0 to 100 and returns the largest one.
 findLargestThree : function(arr) {
   return Math.max.apply(Math, arr);
 },
 
+//Problem 2
 // Write a function that takes a random number from 0 to 100 and returns all the odd numbers from 40 to that random number in an array. If the number is smaller than output the number.
 returnOddArray : function(num) {
   if (num < 40) {
@@ -21,6 +23,7 @@ returnOddArray : function(num) {
   }
 },
 
+//Problem 3
 // Write a function that takes a random number from 0 to 100 and returns all the even numbers from 0 to the randomized number in an array.
 returnEvenArray : function(number) {
   var arr = [];
@@ -32,23 +35,24 @@ returnEvenArray : function(number) {
   return arr;
 },
 
+//Problem 4
 // Write a function that takes a random number from 0 to 100 and returns the all the odd numbers from 40 to that random number in an array. If the number is smaller than 40, return all the numbers down to the randomized one in an array - i.e., if the number is 37, you should return [40, 39, 38, 37].
 returnOddArray2 : function(num) {
   var arr = [];
   if (num < 40) {
-    for (var i = 40; i < (40 - num); i--) {
+    for (var i = 40; i >= num; i--) {
       arr.push(i);
     }
   }
-  if (num > 40) {
-    for (var j = 40; j < (num - 40); j+2) {
+  else if (num > 40) {
+    for (var j = 41; j < num; j+=2) {
       arr.push(j);
     }
   }
   return arr;
 },
 
-
+//Problem 5
 // Write a function that takes a random number from 0 to 10,0000 and returns the sum of its digits. For instance, if the randomized number is 1049, the function should return 14.
 randomizeSum : function(number) {
   var numString = number.toString();
@@ -59,8 +63,8 @@ randomizeSum : function(number) {
   return sum;
 },
 
+//Problem 6
 // Write a function that takes a random number from 0 to 10,0000 and calculates the sum of its digits repeatedly as long as the sum is a single digit. Return that number. For instance, if the randomized number is 1049, the function should return 5.
-
 randomizeSumSingle : function(number) {
   var numString = number.toString();
   var sum = 0;
@@ -73,6 +77,7 @@ randomizeSumSingle : function(number) {
   return sum;
 },
 
+//Problem 7
 // Write a function that takes two random numbers from 0 to 100 and returns the smaller number that is greater than one, such that both numbers are divided by it with no remainder. For instance, for 9 and 6 you should print 3. If there isn't one, simply return "Sorry!".
 divideBy : function(num1, num2) {
   for (var i = 2; i < num1; i++) {
@@ -84,7 +89,7 @@ divideBy : function(num1, num2) {
 //need to add 'Sorry!'
 
 
-
+//Problem 8
 // Write a function that takes two random numbers from 0 to 100 and returns their least common multiple of them.
 findLeastCommon : function(num1, num2) {
   for (var i = num1; i > 0; i--) {
@@ -94,6 +99,7 @@ findLeastCommon : function(num1, num2) {
   }
 },
 
+//Problem 9
 // Write a function that takes two randomized numbers and returns their average and standard deviation.
 
 
